fd402390f1e0629f30cde5a1ae2bd06257fcce2b
John Wick <sarapovaaniuta@gmail.com>
2022-05-03 15:34:04 +0300

    create the application

---
+++ lib/stream_processor.ex
@@ -0,0 +1,18 @@
+defmodule StreamProcessor do
+  @moduledoc """
+  Documentation for `StreamProcessor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StreamProcessor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
